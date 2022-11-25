42d74787d69ae83e91e0f43227cb9c7b92d4170e
theologicalphysicist <adugyamfi3141@gmail.com>
2022-11-25 06:46:33 +0000

    began typescript implementation, due to restrictions of microsoft api

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,5 +1,6 @@
 import Express from "express";
 import {NotionFetch} from "./apis/productivity/notion.js";
+// import {TodoFetch} from "../dist/productivity/microsoft_todo.mjs";
 
 const app = Express();
 
@@ -12,6 +13,12 @@ app.get("/notion_uni_database", async (req, res) => {
     // console.log(NOTION_RESPONSE);
     res.send(JSON.stringify(NOTION_RESPONSE, null, 2));
 });
+
+// app.get("/microsoft_code_courses_lists", async (req, res) => {
+//     const MICROSOFT_RESPONSE = await TodoFetch();
+//     console.log(MICROSOFT_RESPONSE);
+//     res.send(JSON.stringify(MICROSOFT_RESPONSE, null, 2));
+// });
 
 app.listen(process.env.PORT, () => {
     console.log(`App listening on port ${process.env.PORT}`);
